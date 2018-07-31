77a7d7c45202069fa487f5249cdd6387ef4d1c8c
Samir <samirsoormally@gmail.com>
2018-07-31 09:45:05 +0000

    defined methods in directory.rb

---
+++ directory.rb
@@ -0,0 +1,30 @@
+students = [
+	"Dr. Hannibal Lecter",
+	"Darth Vade",
+	"Nurse Rached",
+	"Michael Corleone",
+	"Alex Delarge",
+	"The Wicked Witch of the West",
+	"Terminator",
+	"Freddy Krueger",
+	"The Joker",
+	"Joffrey Baratheon",
+	"Norman Bates"
+]
+
+def print_header
+	puts "The students of Villains Academy"
+	puts "-------------"
+end
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+def print_footer(names)
+	puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
